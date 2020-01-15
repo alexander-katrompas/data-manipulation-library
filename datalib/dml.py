2d90dd51fa73ab8d677349eb4a13fae9170010bd
alexander-katrompas <alexander.katrompas@austincc.edu>
2020-01-15 08:32:34 -0600

    added make_timeseries()

--- datalib/dml.py
+++ datalib/dml.py
@@ -81,14 +81,14 @@ def column_count(data):
         return len(data.columns)
     else:
         return 0
-    
+
 def split_dataset(data, train_pct=DEFAULT_TRAIN_PCT, random=False):
     """
     Parameters: Pandas DataFrame or Numpy array, percent of train data,
                 random sampling flag to determin if taking random
                 percent or first percent 
     Processing: Split data set
-    Return: trianing and test data sets of the same type input
+    Return: Training and test data sets of the same type input
     """
     train_data = None
     test_data = None
@@ -127,3 +127,68 @@ def split_dataset(data, train_pct=DEFAULT_TRAIN_PCT, random=False):
             train_data, test_data = train_test_split(data, train_size=train_pct, shuffle=False)
 
     return train_data, test_data
+
+def make_timeseries(data, out_cols=None, lag=1, fill=False):
+    """
+    Parameters: Pandas DataFrame or Numpy array, list of cols to extract,
+                lag between I/O, fill data
+    Processing: pull out output cols, shift them, append them back to the dataset
+    Return: Dataset with ime shifted output
+    """
+    
+    num_columns = column_count(data)
+    
+    # make sure lag is correct type and preserves at least half of data
+    # else default to lag = 1
+    if type(lag) != type(1) or (lag > int(data.shape[0] / 2) or lag < 1):
+        lag = 1
+    
+    # make sure out_cols is valid
+    if type(out_cols) == type(np.empty(0)) and out_cols.dtype == np.empty(0, dtype=int).dtype:
+        # ensure unique list in order
+        out_cols = np.sort(np.unique(out_cols))
+        # remove any numbers not in range
+        out_cols = out_cols[out_cols >= 0]
+        out_cols = out_cols[out_cols < num_columns]
+        # make sure len(out_cols) is smaller than num_columns
+        out_cols = out_cols[:num_columns-1]
+    else:
+        # assume it's the last col if we don't have a valid list
+        out_cols = [num_columns-1]
+
+    if detect_datatype(data) == DataType.NUMPY:
+        # extract output columns
+        out_data = data[:, out_cols]
+        
+        #created data to insert for shift
+        insert_data = np.empty((lag, column_count(data),))
+        insert_data[:] = np.NaN
+        
+        # add insert data to top
+        data = np.insert(data, 0, insert_data, axis=0)
+        
+        #remove bottom of data
+        data = data[:data.shape[0]-lag,:]
+        
+        # put them together
+        data = np.concatenate((data, out_data), axis=1)
+        
+        if fill: data = np.nan_to_num(data)
+        
+    elif detect_datatype(data) == DataType.DATAFRAME:
+        # extract output columns
+        out_data = data.iloc[:, out_cols]
+
+        #fix col name of out data
+        out_data = out_data.add_suffix('_t-' + str(lag))
+
+        #shift data
+        data = data.shift(lag)
+
+        # put them together
+        data = pd.concat([data, out_data], axis=1)
+
+        if fill: data.fillna(0, inplace=True)
+    
+    return data
+
